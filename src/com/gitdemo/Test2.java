4023d8b8d6eec7501ba77479f22738537e781efe
22Aparna <aparnaiti004@gmail.com>
2022-03-09 20:57:03 +0530

    Added additional checks

--- src/com/gitdemo/Test2.java
+++ src/com/gitdemo/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args)
 	{
 		System.out.println("This is First Git Repository");
+		System.out.println("This is Second Git Repository");
 	}
 
 }
